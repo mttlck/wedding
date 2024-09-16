4cf3feda3bd7995e99a6c501344a46a87c32154b
Matthias Lücke <ml@backslash-n.com>
2024-09-17 00:23:24 +0200

    added trusted proxy
    temp workaround for fast deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: [
+            '172.22.0.0/8', // todo: make configurable via env
+        ]);
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
